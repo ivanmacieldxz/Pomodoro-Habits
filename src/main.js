638a843cc1ee01d4f371a0a44b7bafeacf7d260f
ivanmacieldxz <ivanemanuelmaciel@gmail.com>
2024-03-08 13:28:19 -0300

    Timer, break and stopwatch functionality working Only check as a habit left. Thought about changing the way it works, maybe in an extra section on the pomodoro? With the habit tracker in the same section. Removed reset default button, incorporated its functionality on reset button.

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ trackerButton.addEventListener("click", () => {
     setOnTracker();
 });
 
+//mode change functions
 function setOnPomodoro() {
     tracker.classList.add("hidden");
     pomodoro.classList.remove("hidden");
@@ -19,4 +20,24 @@ function setOnTracker() {
     tracker.classList.remove("hidden");
     pomodoro.classList.add("hidden");
 }
+
+let notification;
+
+//if browser supports notifications
+if ("Notification" in window) {
+    //ask for permission
+    Notification.requestPermission().then((result) => {
+        console.log(result);
+    });
+
+    //remove notifications when tab becomes visible
+    document.addEventListener("visibilitychange", () => {
+        //if there's a notification and tab became visible, clear the notification
+        if (notification != undefined && document.visibilityState === "visible") {
+            notification.close();
+        }
+    })
+
+}
+
 
--- src/pomodoro.js
+++ src/pomodoro.js
@@ -4,11 +4,153 @@ let interval;
 let minutes = 0, seconds = 0, millis = 0;
 const defaultTimer = 25;
 const defaultBreak = 5;
+const minTimer = 10;
+const minBreak = 5;
 let timerConfig = defaultTimer;
 let breakConfig = defaultBreak;
-let stopped = true;
+let started = false, stopped = true;
+let accumulatedTime = 0;
+let lastStopwatchRecord = 0;
+let lastNotification = 0;
 
 updateWatch(defaultTimer);
+
+timerButton.addEventListener("click", () => {
+    mode = "timer";
+    updateWatch(timerConfig);
+    showTimerButtons();
+});
+
+breakButton.addEventListener("click", () => {
+    mode = "break";
+    updateWatch(breakConfig);
+    showTimerButtons();
+});
+
+swButton.addEventListener("click", () => {
+    mode = "stopwatch";
+    updateWatch(0, 0);
+    hideTimerButtons();
+});
+
+//TODO: CHECKER LISTENER
+
+increase.addEventListener("click", () => {
+    if (mode == "timer") {
+        timerConfig++;
+        updateWatch(timerConfig);
+    } else if (mode == "break") {
+        breakConfig++;
+        updateWatch(breakConfig);
+    }
+});
+
+decrease.addEventListener("click", () => {
+    if (mode == "timer") {
+        //if actual time is more than the minimum recommended
+        if (timerConfig > minTimer) {
+            timerConfig--;
+            updateWatch(timerConfig);
+        } else {
+            alert("Sessions under 10 minutes are inefficient!");
+        }
+    } else if (mode == "break") {
+        //if actual break time is not less than recomended
+        if (breakConfig > minBreak){
+            breakConfig--;
+            updateWatch(breakConfig);
+        } else {
+            alert("Take at least 5 minutes to rest!");
+        }
+    }
+});
+
+start.addEventListener("click", () => {
+    //hide start button and show pause button
+    start.classList.add("hidden");
+    pause.classList.remove("hidden");
+    hideTimerButtons();
+
+    if (mode == "stopwatch") {
+        resumeStopwatch();
+    } else {
+        resumeTimer();
+    }
+
+    started = true;
+    stopped = false;
+});
+
+pause.addEventListener("click", () => {
+    //if on pause
+    if (stopped) {
+        //resume stopwatch or timer, accordingly
+        if (mode == "stopwatch") {
+            resumeStopwatch();
+        } else {
+            resumeTimer();
+        }
+
+        //update button text
+        pause.innerText = "Pause";
+    } else {
+        //if not on pause, clear corresponding interval, change text
+        clearInterval(interval);
+        pause.innerText = "Resume";
+
+        //and update time passed
+        updateAccTime();
+    }
+
+    stopped = !stopped;
+});
+
+reset.addEventListener("click", () => {
+
+    //show start button and hide pause button
+    start.classList.remove("hidden");
+    pause.classList.add("hidden");
+
+    //if on execution, clear timer, break or stopwatch
+    if (!stopped) {
+        //clear current interval, whichever it is
+        clearInterval(interval);
+
+        //set as not executing and change pause text
+        stopped = true;
+        pause.innerText = "Pause";
+    }
+
+    if (mode == "timer") {
+        //if not on execution, reset to default configuration
+        if (!started) {
+            timerConfig = defaultTimer;
+        }
+
+        //show increase and decrease buttons
+        showTimerButtons();
+
+        updateWatch(timerConfig, 0);
+    } else if (mode == "break") {
+        //if not on execution, reset to default configuration
+        if (!started) {
+            breakConfig = defaultBreak;
+        }
+
+        //show increase and decrease buttons
+        showTimerButtons();
+
+        updateWatch(breakConfig, 0);
+    } else if (mode == "stopwatch") {
+        //on stopwatch mode, reset to 0
+        updateWatch(0, 0);
+        lastStopwatchRecord = 0;
+        lastNotification = 0;
+    }
+
+    //update started accordingly
+    started = false;
+});
 
 function updateWatch(m, s) {
     minutes = m != undefined ? m : minutes;
@@ -26,132 +168,89 @@ function showTimerButtons() {
     decrease.classList.remove("hidden");
 }
 
-function showDefaultButton() {
-    resetDefault.classList.remove("hidden");
-}
-
-function hideDefaultButton() {
-    resetDefault.classList.add("hidden");
-}
-
-timerButton.addEventListener("click", () => {
-    mode = "timer";
-    updateWatch(timerConfig);
-    showTimerButtons();
-    showDefaultButton();
-});
-
-breakButton.addEventListener("click", () => {
-    mode = "break";
-    updateWatch(breakConfig);
-    showTimerButtons();
-    showDefaultButton();
-});
-
-swButton.addEventListener("click", () => {
-    mode = "stopwatch";
-    updateWatch(0, 0);
-    hideTimerButtons();
-    hideDefaultButton();
-});
-
-//TODO: CHECKER LISTENER
-
-increase.addEventListener("click", () => {
-    if (mode == "timer") {
-        timerConfig++;
-        updateWatch(timerConfig);
-    } else if (mode == "break") {
-        breakConfig++;
-        updateWatch(breakConfig);
-    }
-});
-
-decrease.addEventListener("click", () => {
-    if (mode == "timer") {
-        timerConfig--;
-        updateWatch(timerConfig);
-    } else if (mode == "break") {
-        breakConfig--;
-        updateWatch(breakConfig);
-    }
-});
-
-start.addEventListener("click", () => {
-    //show start button and hide pause button
-    start.classList.add("hidden");
-    pause.classList.remove("hidden");
-    hideTimerButtons();
-
-    resumeInterval();
-    stopped = false;
-});
-
-pause.addEventListener("click", () => {
-    if (stopped) {
-        resumeInterval();
-        pause.innerText = "Pause";
-    }
-    else{
-        clearInterval(interval);
-        pause.innerText = "Resume";
-    }
-
-    stopped = !stopped;
-});
-
-reset.addEventListener("click", () => {
-    //clear current interval, whichever it is
-    clearInterval(interval);
-
-    //show start button and hide pause button
-    start.classList.remove("hidden");
-    pause.classList.add("hidden");
-
-    stopped = true;
-    pause.innerText = "Pause";
-
-    //if on timer mode
-    if (mode == "timer") {
-        //show timer with previous config
-        updateWatch(timerConfig, 0);
-        //show increase, decrease and reset to default config buttons
-        showTimerButtons();
-        showDefaultButton();
-    } else if (mode == "break") {
-        //show break timer with previous config
-        updateWatch(breakConfig, 0);
-        //show increase, decrease and reset to default config buttons
-        showTimerButtons();
-        showDefaultButton();
-    } else if (mode == "stopwatch") {
-        //show stopwatch config
-        updateWatch(0, 0);
-    }
-});
-
-resetDefault.addEventListener("click", () => {
-    if (mode == "timer") {
-        timerConfig = defaultTimer;
-        updateWatch(timerConfig, 0);
-    } else if (mode == "break") {
-        breakConfig = defaultBreak;
-        updateWatch(breakConfig, 0);
-    }
-});
-
-function resumeInterval() {
+function resumeTimer() {
     let end = Date.now() + minutes * 60 * 1000 + seconds * 1000 + millis;
 
     interval = setInterval(() => {
         let actual = Date.now();
         let diff = end - actual;
 
-        updateWatch(
-            Math.floor(diff / 60 / 1000),
-            Math.floor((diff % (60 * 1000)) / 1000)
-        );
+        //if timer finished
+        if (diff < 0) {
+            //clear interval that implements timer
+            clearInterval(interval);
 
-        millis = diff - seconds * 1000 - minutes * 1000 * 60;
+            //show start button and hide pause button
+            start.classList.remove("hidden");
+            pause.classList.add("hidden");
+            
+            let text;
+
+            //show corresponding notif and change to corresponding mode
+            if (mode == "timer") {
+                //reflect results to user
+                updateAccTime();
+
+                text = "Good job! Wanna take a break?";
+                breakButton.click();
+            } else {
+                text = "Time to focus!";
+                timerButton.click();
+            }
+
+            //if notifications are allowed
+            if (Notification.permission == "granted") {
+                notification = new Notification(text);
+            }
+
+            //reset millis counter
+            millis = 0;
+        } else {
+            //if hasn't finished yet, update watch with time decreased
+            updateWatch(
+                Math.floor(diff / 60 / 1000),
+                Math.floor((diff % (60 * 1000)) / 1000)
+            );
+
+            //and update milliseconds track, to avoid innacuracies
+            millis = diff - seconds * 1000 - minutes * 1000 * 60;
+        }
+        
     }, 200);
+}
+
+function resumeStopwatch() {
+    let startTime = Date.now() - millis - seconds * 1000 - minutes * 1000 * 60;
+
+    interval = setInterval(() => {
+        let now = Date.now();
+        let diff = now - startTime;
+
+        updateWatch(
+            Math.floor(diff / 1000 / 60),
+            Math.floor((diff % (1000 * 60)) / 1000)
+        );
+        
+        millis = diff - seconds * 1000 - minutes * 60 * 1000;
+        
+        //if 25 minutes have passed from start or last notification, suggest user to take a break.
+        if (lastNotification != minutes && seconds == 0 && (minutes * 1000) % 25000 == 0) {
+            n = new Notification("More than 25 minutes have passed since last pause. Wanna take a break?\nCurrent time: " + minutes);
+
+            lastNotification = minutes;
+        }
+
+    }, 200);
+}
+
+function updateAccTime() {
+
+    if (mode == "timer") {
+        accumulatedTime += timerConfig - minutes - Math.ceil(seconds / 60);
+    } else if (mode == "stopwatch") {
+        accumulatedTime += minutes - lastStopwatchRecord;
+        lastStopwatchRecord = minutes;
+    }
+
+    timeRecord.innerText = accumulatedTime;
 }
